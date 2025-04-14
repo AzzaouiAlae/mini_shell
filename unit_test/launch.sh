43ffd82df609cc37fb488c1a597b4b1a371a6526
Alae azzaoui <azzaoui@outlook.com>
2025-04-14 16:44:51 +0100

    auto commit

--- unit_test/launch.sh
+++ unit_test/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ../auto_git_push
-# clear
+clear
 gcc -g Unity-master/src/*.c ../mini_shell/*.c ../mini_shell/*/*.c *.c *.h && valgrind --track-origins=yes -s --leak-check=full ./a.out
 #gcc -fsanitize=address  Unity-master/src/*.c ../mini_shell/*.c ../mini_shell/*/*.c *.c  && ./a.out
