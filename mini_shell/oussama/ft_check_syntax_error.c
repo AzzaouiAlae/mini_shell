dd882e0ebd86443d2d4e1bb4558563276408e5d0
Oussama El Banna <oel-bann@e1r8p12.1337.ma>
2025-04-16 17:42:01 +0100

    auto commit

--- mini_shell/oussama/ft_check_syntax_error.c
+++ mini_shell/oussama/ft_check_syntax_error.c
@@ -6,19 +6,49 @@
 /*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 21:27:58 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/15 21:54:10 by oel-bann         ###   ########.fr       */
+/*   Updated: 2025/04/16 17:29:29 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mini_shell.h"
 
-int  check_pipe(t_token token , int i)
+int  check_pipe(t_token **token , int i)
 {
-    if(!i && token.type & e_pipe)
+    if(!i && token[i]->type & e_pipe && (token[i - 1]->type & (e_args | e_cmd))) 
     {
         return(0);
     }
 }
+
+
+int check_redir_in_out_in_first(t_token **token , int i)
+{
+    if (!i && token[i + 1] && !(token[i + 1]->type & e_file_name) )
+    {
+        
+    }
+}
+
+// int check_redir(t_token **token , int i)
+// {
+//     int res;
+
+//     res = g_all.tokens->count - 1 >= i + 1;
+//     if (!i && token[i - 1]->type & e)
+//     if(token[i + 1] && token[i]->type & e_redir_out_trun && !((token[i + 1]->type & e_file_name))) 
+//     {
+//         printf("Minishell : syntax error near unexpected token `>'\n");
+//         g_all.cmd_error_status = 2;
+//         return(0);
+//     }
+//     if(token[i + 1] && token[i]->type & e_redir_in && !((token[i + 1]->type & e_file_name))) 
+//     {
+//         printf("Minishell : syntax error near unexpected token `>'\n");
+//         g_all.cmd_error_status = 2;
+//         return(0);
+//     }
+//     return (1);
+// }
 
 void ft_check_syntax_error()
 {
