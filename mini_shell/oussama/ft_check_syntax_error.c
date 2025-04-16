adfd6c304f079b5945915230f0a169c878089f29
Oussama El Banna <oel-bann@e1r8p12.1337.ma>
2025-04-17 00:34:40 +0100

    auto commit

--- mini_shell/oussama/ft_check_syntax_error.c
+++ mini_shell/oussama/ft_check_syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 22:17:25 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/16 22:25:26 by oel-bann         ###   ########.fr       */
+/*   Updated: 2025/04/16 23:57:32 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,22 +39,26 @@ int check_redir_in_out_in_first(t_token **token , int i)
 
 int check_redir(t_token **token , int i)
 {
-    int res;
+    int after_cmd, (berfor_cmd);
 
-    res = g_all.tokens->count - 1 >= i + 1;
-    //if (!i && token[i - 1]->type & e)
-    if(token[i + 1] && token[i]->type & e_redir_out_trun && !((token[i + 1]->type & e_file_name))) 
-    {
-        printf("Minishell : syntax error near unexpected token `>'\n");
-        g_all.cmd_error_status = 2;
-        return(0);
-    }
-    if(token[i + 1] && token[i]->type & e_redir_in && !((token[i + 1]->type & e_file_name))) 
-    {
-        printf("Minishell : syntax error near unexpected token `>'\n");
-        g_all.cmd_error_status = 2;
-        return(0);
-    }
+    berfor_cmd = (i > 0) && (e_args | e_pipe | e_file_name) & token[i - 1]->type;
+    after_cmd = token[i + 1] &&  (e_file_name | e_set_var ) & token[i + 1]->type;
+    if(token[i]->type & e_redir_out_trun && !berfor_cmd)
+        return(print_redir_error(token[i - 1]->s), 0);
+    else if(token[i]->type & e_redir_out_trun && !after_cmd)
+        return(print_redir_error(token[i + 1]->s), 0);
+    if(token[i]->type & e_redir_in && !berfor_cmd)
+        return(print_redir_error(token[i - 1]->s), 0);
+    else if(token[i]->type & e_redir_in && !after_cmd)
+        return(print_redir_error(token[i + 1]->s), 0);
+    if(token[i]->type & e_redir_out_app && !berfor_cmd)
+        return(print_redir_error(token[i - 1]->s), 0);
+    else if(token[i]->type & e_redir_out_app && !after_cmd)
+        return(print_redir_error(token[i + 1]->s), 0);
+    if(token[i]->type & e_redir_out_trun && !berfor_cmd)
+        return(print_redir_error(token[i - 1]->s), 0);
+    else if(token[i]->type & e_redir_out_trun && !after_cmd)
+        return(print_redir_error(token[i + 1]->s), 0);
     return (1);
 }
 
