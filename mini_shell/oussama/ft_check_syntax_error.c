ce1c94c7c9f84180f0cc8bef83b5f9c5b2aa7232
Oussama El Banna <oel-bann@e1r8p12.1337.ma>
2025-04-16 19:07:20 +0100

    auto commit

--- mini_shell/oussama/ft_check_syntax_error.c
+++ mini_shell/oussama/ft_check_syntax_error.c
@@ -6,28 +6,33 @@
 /*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 21:27:58 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/16 17:29:29 by oel-bann         ###   ########.fr       */
+/*   Updated: 2025/04/16 19:06:28 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../mini_shell.h"
+#include "ft_check_syntax.h"
 
-int  check_pipe(t_token **token , int i)
-{
-    if(!i && token[i]->type & e_pipe && (token[i - 1]->type & (e_args | e_cmd))) 
-    {
-        return(0);
-    }
-}
-
-
-int check_redir_in_out_in_first(t_token **token , int i)
-{
-    if (!i && token[i + 1] && !(token[i + 1]->type & e_file_name) )
-    {
+// int  check_pipe(t_token **token , int i)
+// {
+//     if(!i && token[i]->type & e_pipe && (token[i - 1]->type & (e_args | e_cmd))) 
         
-    }
-}
+// }
+
+
+// int check_redir_in_out_in_first(t_token **token , int i)
+// {
+//     if (token[i]->type & e_redir_out_trun && !token[i + 1])
+//         return(print_redir_error("newline"), 0);
+//     if (!i && token[i + 1] &&  token[i]->type & e_redir_out_trun && !(token[i + 1]->type & e_file_name))
+//         return(print_redir_error(token[i + 1]), 0);
+//     if (!i && token[i + 1] && token[i]->type & e_redir_in && !(token[i + 1]->type & e_file_name))
+//         return(print_redir_error(token[i + 1]), 0);
+//     if (!i && token[i + 1] &&  token[i]->type & e_redir_out_app && !(token[i + 1]->type & e_file_name))
+//         return(print_redir_error(token[i + 1]), 0);
+//     if (!i && token[i + 1] && token[i]->type & e_heredoc && !(token[i + 1]->type & e_file_name))
+//         return(print_redir_error(token[i + 1]), 0);
+//     return(1);
+// }
 
 // int check_redir(t_token **token , int i)
 // {
@@ -50,17 +55,18 @@ int check_redir_in_out_in_first(t_token **token , int i)
 //     return (1);
 // }
 
-void ft_check_syntax_error()
-{
-    t_token **token;
-    int i;
+// void ft_check_syntax_error()
+// {
+//     t_token **token;
+//     int i;
     
-    token = g_all.tokens->content;
-    g_all.cmd_error_status = 0;
-    i = 0;
-    while (token[i])
-    {
-        
-        i++;
-    }
-}
+//     token = g_all.tokens->content;
+//     g_all.cmd_error_status = 0;
+//     i = 0;
+//     while (token[i])
+//     {
+//         if (check_redir_in_out_in_first(token, i) == 0)
+//             return(0);
+//         i++;
+//     }
+// }
