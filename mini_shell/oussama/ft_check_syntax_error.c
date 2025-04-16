571f84c8fecc55906a3b42cff0e73c22952f89ba
Oussama El Banna <oel-bann@e1r8p12.1337.ma>
2025-04-16 22:15:21 +0100

    auto commit

--- mini_shell/oussama/ft_check_syntax_error.c
+++ mini_shell/oussama/ft_check_syntax_error.c
@@ -6,7 +6,15 @@
 /*   By: aazzaoui <aazzaoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 21:27:58 by oel-bann          #+#    #+#             */
+<<<<<<< HEAD
+<<<<<<< HEAD
 /*   Updated: 2025/04/16 21:41:29 by aazzaoui         ###   ########.fr       */
+=======
+/*   Updated: 2025/04/16 19:55:49 by oel-bann         ###   ########.fr       */
+>>>>>>> ba44484 (auto commit)
+=======
+/*   Updated: 2025/04/16 19:55:49 by oel-bann         ###   ########.fr       */
+>>>>>>> main
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +22,32 @@
 
 int  check_pipe(t_token **token , int i)
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
     if(!i && token[i]->type & e_pipe && (token[i - 1]->type & (e_args | e_cmd))) 
     {
         
     }
+=======
+=======
+>>>>>>> main
+    if(!i && token[i]->type & e_pipe && (token[i + 1] || !token[i + 1])) 
+        return(print_redir_error("|"), 0);
+    if(token[i + 1] && token[i]->type & e_pipe && (token[i + 1]->type & e_pipe))
+        return(print_redir_error("|"), 0);
+    return(1);
+    0000 0010  0000 0010
+<<<<<<< HEAD
+>>>>>>> ba44484 (auto commit)
+=======
+>>>>>>> main
 }
 
 
 int check_redir_in_out_in_first(t_token **token , int i)
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
     // if (token[i]->type & e_redir_out_trun && !token[i + 1])
     //     return(print_redir_error("newline"), 0);
     // if (!i && token[i + 1] &&  token[i]->type & e_redir_out_trun && !(token[i + 1]->type & e_file_name))
@@ -33,6 +58,23 @@ int check_redir_in_out_in_first(t_token **token , int i)
     //     return(print_redir_error(token[i + 1]), 0);
     // if (!i && token[i + 1] && token[i]->type & e_heredoc && !(token[i + 1]->type & e_file_name))
     //     return(print_redir_error(token[i + 1]), 0);
+=======
+=======
+>>>>>>> main
+    if (token[i]->type & e_redir_out_trun && !token[i + 1])
+        return(print_redir_error("newline"), 0);
+    if (!i && token[i + 1] &&  token[i]->type & e_redir_out_trun && !(token[i + 1]->type & e_file_name))
+        return(print_redir_error(token[i + 1]), 0);
+    if (!i && token[i + 1] && token[i]->type & e_redir_in && !(token[i + 1]->type & e_file_name))
+        return(print_redir_error(token[i + 1]), 0);
+    if (!i && token[i + 1] &&  token[i]->type & e_redir_out_app && !(token[i + 1]->type & e_file_name))
+        return(print_redir_error(token[i + 1]), 0);
+    if (!i && token[i + 1] && token[i]->type & e_heredoc && !(token[i + 1]->type & e_file_name))
+        return(print_redir_error(token[i + 1]), 0);
+<<<<<<< HEAD
+>>>>>>> ba44484 (auto commit)
+=======
+>>>>>>> main
     return(1);
 }
 
