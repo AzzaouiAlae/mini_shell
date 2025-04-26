ade216ec741e8ba5ba8dd555f683ae1be3ae3cb2
Oussama El Banna <oel-bann@c4r2p9.1337.ma>
2025-04-26 19:40:20 +0100

    auto commit

--- mini_shell/oussama/check_syntax_error/ft_check_syntax_error.c
+++ mini_shell/oussama/check_syntax_error/ft_check_syntax_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_syntax_error.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aazzaoui <aazzaoui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 22:17:25 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/26 16:38:11 by aazzaoui         ###   ########.fr       */
+/*   Updated: 2025/04/25 03:56:57 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int check_redir_in_out_in_first(t_token **token , int i)
         return(print_redir_error(token[i + 1]->s), 0);
     if (!i && token[i + 1] &&  token[i]->type & e_redir_out_app && !(token[i + 1]->type & e_file_name))
         return(print_redir_error(token[i + 1]->s), 0);
-    if (!i && token[i + 1] && token[i]->type & e_heredoc && !(token[i + 1]->type & e_file_name))
+    if (!i && token[i + 1] && token[i]->type & e_heredoc && !(token[i + 1]->type & e_delimiter))
         return(print_redir_error(token[i + 1]->s), 0);
     return(1);
 }
@@ -43,7 +43,7 @@ int check_redir(t_token **token , int i)
     int after_cmd, (berfor_cmd), (last);
 
     berfor_cmd = (i > 0) && (e_args | e_pipe | e_file_name) & token[i - 1]->type;
-    after_cmd = token[i + 1] && (e_file_name | e_set_var ) & token[i + 1]->type;
+    after_cmd = token[i + 1] && (e_file_name | e_set_var | e_delimiter) & token[i + 1]->type;
     last = (e_redir_out_app | e_redir_out_trun | e_redir_in | e_heredoc) & token[i]->type;
     if (last && !token[i + 1])
         return(print_redir_error("newline"), 0);
@@ -59,7 +59,7 @@ int check_redir(t_token **token , int i)
         return(print_redir_error(token[i - 1]->s), 0);
     else if(token[i]->type & e_redir_out_app && !after_cmd)
         return(print_redir_error(token[i + 1]->s), 0);
-    if(token[i]->type & e_heredoc && !berfor_cmd)
+    if(token[i]->type & e_heredoc && (i > 0) && !berfor_cmd)
         return(print_redir_error(token[i - 1]->s), 0);
     else if(token[i]->type & e_heredoc && !after_cmd)
         return(print_redir_error(token[i + 1]->s), 0);
--- mini_shell/oussama/herdoc/herdoc.c
+++ mini_shell/oussama/herdoc/herdoc.c
@@ -3,32 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   herdoc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aazzaoui <aazzaoui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 11:16:53 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/18 22:40:12 by aazzaoui         ###   ########.fr       */
+/*   Updated: 2025/04/25 04:37:45 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "her_doc.h"
 
-void	ft_limiter_rem(int i, char *filename)
+char *here_doc(char *delimiter, int i)
 {
-	cs_list_delete(g_all.tokens, i);
-    cs_list_delete(g_all.tokens, i - 1);
-    cs_list_inset_at(g_all.tokens, i - 1, (long)filename);
-}
-
-int	here_doc(t_token **tokens, int i)
-{
+	static 
 	int (fd);
 	char *(limiter), *(str), *(file_name);
 	file_name = create_file_name();
 	fd = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0666);
 	if (fd == -1)
-		return (perror("ERROR HERDOC FILE"), ft_exit(127), 0);
-	str = readline(">");
-	limiter = ft_strjoin(tokens[i]->s, "\n");
+		return (perror("ERROR HERDOC FILE"), ft_exit(127), NULL);
+	str = readline("$>: ");
+	limiter = ft_strjoin(delimiter, "\n");
 	while (ft_strncmp(limiter, str, ft_strlen(limiter)) != 0
 		|| ft_strncmp(limiter, str, ft_strlen(limiter) - 1) != 0)
 	{
@@ -40,9 +34,14 @@ int	here_doc(t_token **tokens, int i)
         }
 		write(fd, str, ft_strlen(str));
 		free(str);
-		str = readline(">");;
+		str = readline("$>: ");;
 	}
 	free(str);
-	ft_limiter_rem(i, file_name);
-	return (0);
+	return (file_name);
+}
+
+
+int main()
+{
+	
 }
