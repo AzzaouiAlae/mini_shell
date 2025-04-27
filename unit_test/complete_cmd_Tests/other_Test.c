5d5815f79926a6a4ce36869a4ebbb206d0c80cfe
Alae azzaoui <azzaoui@outlook.com>
2025-04-27 16:48:19 +0100

    auto commit

--- unit_test/complete_cmd_Tests/other_Test.c
+++ unit_test/complete_cmd_Tests/other_Test.c
@@ -3,6 +3,7 @@
 /*
 echo $HOME$ '$'$'$'
 echo $HOME$ $hey $hey '$'
+
 echo $HOME$ $P $ $P  $P '$'
 echo $HOME$ "$P'$' $PWD $USER $ P $P
 echo $<b "'$'$'$'" '$PWD'
@@ -28,9 +29,163 @@ void OthetTest2()
 {
     //Arrange
     char *input = "echo $HOME$ $hey $hey '$'";
-    char *args[] = {"echo", "/home/aazzaoui$", "", "", "$", 0};
+    char *args[] = {"echo", "/home/aazzaoui$", "$", 0};
     t_cmd cmd[] = {
         { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest3()
+{
+    //Arrange
+    char *input = "echo $HOME$ $hey $hey '$'";
+    char *args[] = {"echo", "/home/aazzaoui$", "$", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest4()
+{
+    //Arrange
+    char *input = "echo $H $H $H $H hhhh";
+    char *args[] = {"echo", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest5()
+{
+    //Arrange
+    char *input = "echo \"$H\" \"$H\" \"$H\" \"$H\" hhhh";
+    char *args[] = {"echo", "", "", "", "", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest6()
+{
+    //Arrange
+    char *input = "echo \"$H\"  hhhh";
+    char *args[] = {"echo", "", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest7()
+{
+    //Arrange
+    char *input = "echo \"$H\" \"$H\" hhhh";
+    char *args[] = {"echo", "", "", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest8()
+{
+    //Arrange
+    char *input = "echo \"$H\" \"$H\" \"$H\" hhhh";
+    char *args[] = {"echo", "", "", "", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest9()
+{
+    //Arrange
+    char *input = "echo \"$H\" \"$H\" \"$H\" \"$H\" hhhh";
+    char *args[] = {"echo", "", "", "", "", "hhhh", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest10()
+{
+    //Arrange
+    char *input = "echo $HOME$ $P $ $P  $P '$'";
+    char *args[] = {"echo", "/home/aazzaoui$", "$", "$", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest11()
+{
+    //Arrange
+    char *input = "echo $ \"'$'$'$'\" '$PWD'";
+    char *args[] = {"echo", "$", "'$'$'$'", "$PWD", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 }, 
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest12()
+{
+    //Arrange
+    char *input = "echo $$b";
+    char *args[] = {"echo", "$$b", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 },
+        0
+    };
+
+    //act  //ASSERT
+    builtins_complete(input, cmd, 1);
+}
+
+void OthetTest13()
+{
+    //Arrange
+    char *input = "echo $$b";
+    char *args[] = {"echo", "$$b", 0};
+    t_cmd cmd[] = {
+        { "echo", args, 0, 0, 0, 0, 0 },
         0
     };
 
@@ -42,4 +197,14 @@ void OthetTests()
 {
     RUN_TEST(OthetTest1);
     RUN_TEST(OthetTest2);
+    RUN_TEST(OthetTest3);
+    RUN_TEST(OthetTest4);
+    RUN_TEST(OthetTest5);
+    RUN_TEST(OthetTest6);
+    RUN_TEST(OthetTest7);
+    RUN_TEST(OthetTest8);
+    RUN_TEST(OthetTest9);
+    RUN_TEST(OthetTest10);
+    RUN_TEST(OthetTest11);
+    RUN_TEST(OthetTest12);
 }
