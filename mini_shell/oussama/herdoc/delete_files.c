9e3d1c7340623e5f214e0934064d0d1f6cb2c9e1
Oussama El Banna <oel-bann@c4r2p9.1337.ma>
2025-04-27 19:48:17 +0100

    auto commit

--- mini_shell/oussama/herdoc/delete_files.c
+++ mini_shell/oussama/herdoc/delete_files.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   her_doc.h                                          :+:      :+:    :+:   */
+/*   delete_files.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oel-bann <oel-bann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/18 12:42:26 by oel-bann          #+#    #+#             */
-/*   Updated: 2025/04/27 19:48:09 by oel-bann         ###   ########.fr       */
+/*   Created: 2025/04/27 19:42:44 by oel-bann          #+#    #+#             */
+/*   Updated: 2025/04/27 19:47:38 by oel-bann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HER_DOC_H
-#define HER_DOC_H
+#include "her_doc.h"
 
-#include "../../mini_shell.h"
-
-
-typedef struct t_her_doc
-{
-    char    *limiter;
-    char    *str;
-    char    *file_name;
-    char    *expand_str;
-    int     expand_her;
-    int     fd;
-} t_her_doc;
-
-char	*create_file_name(void);
-int here_doc(t_token **tokens, int i, int expand_her);
-
-#endif
+// void delete_files()
+// {
+    
+// }
