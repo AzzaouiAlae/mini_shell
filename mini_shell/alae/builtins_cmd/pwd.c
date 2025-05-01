a5d158905dc951a8ae4a7bca29827d60894c196e
Oussama El Banna <oel-bann@e1r8p12.1337.ma>
2025-05-01 21:58:07 +0100

    auto commit

--- mini_shell/alae/builtins_cmd/pwd.c
+++ mini_shell/alae/builtins_cmd/pwd.c
@@ -4,7 +4,6 @@ void pwd(t_cmd *cmd)
 {
     t_cs_list *value;
     value = cpp_map_get(g_all.custom_env, "PWD");
-    cpp_map_add(g_all.custom_env, "OLDPWD", value->content);
-    
-    printf("this command is not implemented yet\n");
+    printf("%s\n",(char *)value->content);
+    // cpp_map_add(g_all.custom_env, "OLDPWD", value->content);
 }
